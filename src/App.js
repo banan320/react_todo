103f37453e278e067f19f4bf3dcf2614309ae011
Alex Gerber <alex-gerber@mail.ru>
2023-02-09 17:52:39 +0300

    edits

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import { v4 as uuidv4 } from "uuid";
 import { randomColor } from "randomcolor";
 import Draggable from "react-draggable";
+import { FaClipboardList } from "react-icons/fa";
+import { TbTools } from "react-icons/tb";
+import { MdTaskAlt } from "react-icons/md";
 
 import { useState, useEffect } from "react";
 import "./App.css";
@@ -26,8 +29,8 @@ function App() {
           luminosity: "light",
         }),
         defaultPos: {
-          x: 100,
-          y: 100,
+          x: 0,
+          y: 0,
         },
       };
       setLocalItem((localItem) => [...localItem, newElem]);
@@ -58,36 +61,64 @@ function App() {
   return (
     <div className="App">
       <div className="wrapper">
-        <input
-          value={item}
-          type="text"
-          placeholder="Добавить задачу..."
-          onChange={(e) => setItem(e.target.value)}
-          onKeyPress={(e) => keyPress(e)}
-        />
-        <button className="enter" type="button" onClick={newItem}>
-          Добавить
-        </button>
-        <ul className="todo__item">
-          {localItem.map((el, index) => {
-            return (
-              <Draggable
-                defaultPosition={el.defaultPos}
-                key={index}
-                onStop={(_, data) => {
-                  updatePos(data, index);
-                }}
-              >
-                <li style={{ backgroundColor: el.color }}>
-                  {`${el.task}`}{" "}
-                  <button className="delete" onClick={() => deleteNode(el.id)}>
-                    X
-                  </button>
-                </li>
-              </Draggable>
-            );
-          })}
-        </ul>
+        <div className="container">
+          <div className="todo__form">
+            <input
+              value={item}
+              type="text"
+              placeholder="Добавить задачу..."
+              onChange={(e) => setItem(e.target.value)}
+              onKeyPress={(e) => keyPress(e)}
+            />
+            <button className="enter" type="button" onClick={newItem}>
+              Добавить
+            </button>
+          </div>
+          <div className="todo__steps">
+            <div className="steps__title">
+              <div className="title__steps">
+                <h2>Новые задачи</h2>{" "}
+                <FaClipboardList className="react__icons" />
+              </div>
+              <div className="title__steps">
+                <h2>В процессе</h2> <TbTools className="react__icons" />
+              </div>
+              <div className="title__steps">
+                <h2>Завершенные</h2> <MdTaskAlt className="react__icons" />
+              </div>
+            </div>
+            <div className="steps__block">
+              <div className="steps__new">
+                <ul className="todo__item">
+                  {localItem.map((el, index) => {
+                    return (
+                      <Draggable
+                        bounds=".steps__block"
+                        position={el.defaultPos}
+                        key={index}
+                        onStop={(_, data) => {
+                          updatePos(data, index);
+                        }}
+                      >
+                        <li style={{ backgroundColor: el.color }}>
+                          {`${el.task}`}
+                          <button
+                            className="delete"
+                            onClick={() => deleteNode(el.id)}
+                          >
+                            X
+                          </button>
+                        </li>
+                      </Draggable>
+                    );
+                  })}
+                </ul>
+              </div>
+              <div className="steps__started"></div>
+              <div className="steps__completed"></div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
